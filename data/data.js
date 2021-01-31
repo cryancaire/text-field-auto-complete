e98cfbb51c2ff07a1262d98ea612903013e6f3bb
Corey Caire <cryancaire@gmail.com>
2021-01-30 22:45:53 -0600

    Initial Commit

---
+++ data/data.js
@@ -0,0 +1,25 @@
+export function getMyData () {
+    return ["Seth",
+            "Devin",
+            "Kennedy",
+            "Valentine",
+            "Jasper",
+            "Stone",
+            "Jack",
+            "Benedict",
+            "Jonah",
+            "Jason",
+            "Colorado",
+            "Allen",
+            "Neville",
+            "Bruno",
+            "Ira",
+            "Baker",
+            "Dillon",
+            "Dolan",
+            "Hyatt",
+            "Elliott",
+            "Laith",
+            "Corey"
+        ]
+}
---
+++ searchAutoComplete.js
@@ -0,0 +1,83 @@
+//Get the data from whereever you want
+import { getMyData } from '../data/data.js';
+let theData = getMyData();
+theData = theData.sort();
+
+//get the input to use
+//and set up some elements
+const inputToAutoComplete = document.querySelectorAll('[data-auto-complete="true"]');
+let resultDiv = document.createElement('div');
+let resultListUl = document.createElement('ul');
+resultDiv.classList.add('__results-div');
+resultListUl.classList.add('__results-ul');
+
+inputToAutoComplete.forEach((item) => {
+    //wrap the input field in a span
+    //to make sure it all works correctly
+    let parent = item.parentNode;
+    let wrapper = document.createElement('span');
+    parent.replaceChild(wrapper, item);
+    wrapper.appendChild(item)
+    wrapper.classList.add('__auto-complete-wrapper');
+
+    resultDiv.style.width = `${item.clientWidth}px`;
+    item.classList.add('__auto-complete-input');
+    let result;
+    item.addEventListener("keyup", (e) => {
+        processEvent(item, result);
+    });
+
+    item.addEventListener("click", () => {
+        processEvent(item, result);
+    });
+
+    item.addEventListener("blur", () => {
+        resultDiv.classList.add('__results-div_hide');
+    });
+});
+
+
+//Used to filter out all non matches
+let matchData = (input, dataSet) => {
+    return dataSet.filter(element => element.toLowerCase().startsWith(input.toLowerCase()));
+}
+
+//Used to return a default string
+//when no results are found
+let displayNoResults = () => {
+    return [ "No results" ];
+}
+
+//Unused currently, but would be used 
+//to just return all results
+let displayAllData = (dataSet) => {
+    return dataSet;
+}
+
+//Kind of dirty/messy, used to display the list
+//also had to add click listener for each item
+let displayData = (results, element) => {
+    resultListUl.innerHTML="";
+    results.forEach((item) => {
+        resultListUl.innerHTML += `<li class="__results-list-item" data-results-item="true">${item}</li>`;
+    });
+    resultDiv.append(resultListUl);
+    element.parentElement.append(resultDiv);
+
+    let eachResultItem = document.querySelectorAll('[data-results-item="true"]');
+    eachResultItem.forEach((item) => {
+        item.addEventListener('mousedown', () => {
+            element.value = `${item.innerHTML}`;
+        });
+    });
+}
+
+//event for both click and typing
+let processEvent = (item, result) => {
+    resultDiv.classList.remove('__results-div_hide');
+    result=matchData(item.value, theData);
+    if (result.length < 1) {
+        result=displayNoResults();
+    }
+    displayData(result, item);
+}
